9b508703048954b5516d634c06e9a04257afef87
luowanshun <luowanshun@gmail.com>
2020-03-06 17:46:13 +0800

    set use_context = True

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ from config import TOKEN, APP_KEY, APP_SECRET
 app = Flask(__name__)
 
 bot = telegram.Bot(token=TOKEN)
-dispatcher = Dispatcher(bot,None)
+dispatcher = Dispatcher(bot,None, use_context=True)
 
 @app.route('/', methods=['POST'])
 def handle():
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         dispatcher.process_update(update)
+        return 'ok', 200
     return 'successful'
 
 def trans(update, context):
